d25eace0d94ad0129b8b0c092a10ccbd46afe4e4
Danilo Uy <danilouy2011@gmail.com>
2020-12-20 10:50:25 -0500

    first commit

--- middleware/headers.js
+++ middleware/headers.js
@@ -4,7 +4,7 @@ module.exports = function (req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 //  res.header('Access-Control-Allow-Headers', '*')
-    res.header('Access-Control-Allow-Credentials', true)
+//  res.header('Access-Control-Allow-Credentials', true)
     next();
 };
  // CORS - Cross Origin Resource Sharing: mechanism that uses additional HTTP Headers to tell browsers to give a web application that is running at one origin, access to
