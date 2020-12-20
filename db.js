dc582c2d87b13f8dd29c89b4fd2cee8fe32cd15e
Danilo Uy <danilouy2011@gmail.com>
2020-12-20 10:58:48 -0500

    first commit

--- db.js
+++ db.js
@@ -1,17 +1,34 @@
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize('workout-log2',
+// 'postgres', 'Letmein1234!', {
+//     host: 'localhost',
+//     dialect: 'postgres'
+// });
+
+// sequelize.authenticate().then(
+//     function() {
+//         console.log ("Connected to workout-log2 postgres database");
+//     },
+//     function(err) {
+//         console.log(err);
+//     }
+// );
+
+// module.exports = sequelize;
+
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('workout-log2',
-'postgres', 'Letmein1234!', {
-    host: 'localhost',
-    dialect: 'postgres'
-});
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+   dialect: 'postgres',
+})
 
 sequelize.authenticate().then(
-    function() {
-        console.log ("Connected to workout-log2 postgres database");
-    },
-    function(err) {
-        console.log(err);
-    }
+  function() {
+    console.log('Connected to the Workout Log Database!');
+  },
+  function(err){
+    console.log(err);
+  }
 );
 
 module.exports = sequelize;
